9b40fd76c020519f8b4a0e6ccb4093123bb91bfb
mohit kumar <mohitrajput914474@gmail.com>
2025-08-16 18:09:26 +0530

    best

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+
 const app = express();
 
 
@@ -14,6 +15,10 @@ app.use(cors(
         credentials: true
     }
 ));
+app.use(express.json( {limit: '20kb'} ));
+app.use(express.urlencoded({ extended: true, limit: '20kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
 
 
